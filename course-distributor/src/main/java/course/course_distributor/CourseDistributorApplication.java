93c4cc3a7a1c0ab0558255e81ea5d077460e28df
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-14 01:14:41 +0530

    initial commit: backend setup

---
+++ course-distributor/src/main/java/course/course_distributor/CourseDistributorApplication.java
@@ -0,0 +1,13 @@
+package course.course_distributor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseDistributorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseDistributorApplication.class, args);
+	}
+
+}
