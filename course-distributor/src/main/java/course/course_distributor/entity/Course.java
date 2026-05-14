f0f860c178a30a5ce67b8a1384476d0e0504e732
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-14 17:21:48 +0530

    feat: create Course entity

---
+++ course-distributor/src/main/java/course/course_distributor/entity/Course.java
@@ -0,0 +1,28 @@
+package course.course_distributor.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course{
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    private Set<User> likes;
+
+}
