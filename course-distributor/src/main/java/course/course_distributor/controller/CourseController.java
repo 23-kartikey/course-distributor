f38d754e9abb7395003b96c8d7e57e91978e58b4
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-14 21:05:12 +0530

    feat: add endpoint to get all courses

--- course-distributor/src/main/java/course/course_distributor/controller/CourseController.java
+++ course-distributor/src/main/java/course/course_distributor/controller/CourseController.java
@@ -1,7 +1,10 @@
 package course.course_distributor.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,11 @@ public class CourseController {
 
     @Autowired
     private CourseService service;
+
+    @GetMapping
+    public ResponseEntity<List<CourseResponse>> getCourses(){
+        return ResponseEntity.ok(service.getCourses());
+    }
 
     @PostMapping
     public ResponseEntity<CourseResponse> newCourse(@RequestBody CourseRequest req){
