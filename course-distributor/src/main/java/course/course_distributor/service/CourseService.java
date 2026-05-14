3bdf3d728ac442f405746280d3f6dd9b5bea6473
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-14 18:11:55 +0530

    feat: create Course Service class

---
+++ course-distributor/src/main/java/course/course_distributor/service/CourseService.java
@@ -0,0 +1,36 @@
+package course.course_distributor.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import course.course_distributor.dto.CourseRequest;
+import course.course_distributor.dto.CourseResponse;
+import course.course_distributor.entity.Course;
+import course.course_distributor.repository.CourseRepository;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepo;
+
+    public CourseResponse newCourse(CourseRequest req){
+        return toResponse(courseRepo.save(toCourse(req)));
+    }
+    
+    private Course toCourse(CourseRequest req){
+        return Course
+            .builder()
+            .name(req.name())
+            .build();
+    }
+
+    private CourseResponse toResponse(Course course){
+        return CourseResponse
+            .builder()
+            .id(course.getId())
+            .name(course.getName())
+            .build();
+    }
+
+}
