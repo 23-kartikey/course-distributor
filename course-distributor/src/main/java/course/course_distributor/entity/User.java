6c99be30fe97f15625f6a3f54225229c3f3cfb35
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-14 18:18:32 +0530

    feat: add generation type to identity

--- course-distributor/src/main/java/course/course_distributor/entity/User.java
+++ course-distributor/src/main/java/course/course_distributor/entity/User.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
@@ -21,6 +23,7 @@ import lombok.Setter;
 public class User {
     
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
     private String name;
