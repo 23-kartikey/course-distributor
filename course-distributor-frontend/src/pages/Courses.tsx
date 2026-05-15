6cdc4f7078442b1c667915e57b4633efb5b90545
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-15 21:22:19 +0530

    feat: add components to it and add styles to it

--- course-distributor-frontend/src/pages/Courses.tsx
+++ course-distributor-frontend/src/pages/Courses.tsx
@@ -1,36 +1,13 @@
-import { useState, useEffect, type SubmitEventHandler } from "react";
-import { getCourses, createCourse } from "../services/CourseService";
-import type { Course } from "../types/course";
 import CourseForm from "../components/CourseForm";
+import CourseList from "../components/CourseList";
+import "../styles/Courses.css";
 
 const Courses = () =>{
     
-    const [courses, setCourses]= useState<Course[]>([]);
-
-    useEffect(()=>{
-        
-        const loadCourses = async() => {
-            try{
-                const data = await getCourses();
-                setCourses(data);
-            }
-            catch(error){
-                console.log("Unable to fetch courses: " + error);
-            }
-        };
-
-        loadCourses();
-    }, [])
 
     return(
-        <div>
-            {courses.map(course=>(
-                <ul key={course.id}>
-                    <li>
-                        <p><strong>{course.name}</strong>  {course.description}</p>
-                    </li>
-                </ul>
-            ))}
+        <div className="courses-page">
+            <CourseList />
             <CourseForm />
         </div>
     );
