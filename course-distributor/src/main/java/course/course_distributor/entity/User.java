c795cede9912b3dc2e69afb65c2e7562cdd09753
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-16 00:38:40 +0530

    feat: add more attributes

--- course-distributor/src/main/java/course/course_distributor/entity/User.java
+++ course-distributor/src/main/java/course/course_distributor/entity/User.java
@@ -30,6 +30,14 @@ public class User {
 
     private String name;
 
+    private String username;
+
+    private String email;
+
+    private String password;
+
+
+
 
     @ManyToMany
     @JoinTable(
