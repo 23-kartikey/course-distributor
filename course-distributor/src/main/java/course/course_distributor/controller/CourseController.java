95704b8bbf1804c8cc4107990bc16d96b22137f6
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-15 18:53:29 +0530

    feat: add CORS

--- course-distributor/src/main/java/course/course_distributor/controller/CourseController.java
+++ course-distributor/src/main/java/course/course_distributor/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import course.course_distributor.dto.CourseResponse;
 import course.course_distributor.service.CourseService;
 
 @RestController
+@CrossOrigin(origins="http://localhost:5173")
 @RequestMapping("/course")
 public class CourseController {
 
