227554b591fbf2c3b0cf88f2b44df6fb83988ac0
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-16 00:40:41 +0530

    feat: add method implementation for authentication

--- course-distributor/src/main/java/course/course_distributor/repository/UserRepository.java
+++ course-distributor/src/main/java/course/course_distributor/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package course.course_distributor.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import course.course_distributor.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
     
+    public Optional<User> findByUsernameOrEmail(String username, String email);
+
 }
