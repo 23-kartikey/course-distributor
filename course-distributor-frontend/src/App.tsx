9f14f9562c07f47c18261ff12064b7d57776ff3f
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-15 18:49:08 +0530

    feat: added Routes

---
+++ course-distributor-frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Courses from "./pages/Courses";
+import Login from "./pages/Login";
+
+const App = () => {
+    return(
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/courses" element={<Courses />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
