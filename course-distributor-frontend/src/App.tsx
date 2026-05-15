0667893e67e99665e8dea9512cd49ea40ea42461
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-15 21:20:51 +0530

    feat: add new pages

--- course-distributor-frontend/src/App.tsx
+++ course-distributor-frontend/src/App.tsx
@@ -4,10 +4,12 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Courses from "./pages/Courses";
 import Login from "./pages/Login";
+import Header from "./components/Header"
 
 const App = () => {
     return(
         <BrowserRouter>
+            <Header />
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
