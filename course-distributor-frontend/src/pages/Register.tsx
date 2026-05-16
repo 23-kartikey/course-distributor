1f58d35c6400e94303e7fc2d59e86a60a0886daa
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-17 04:57:06 +0530

    feat: created Register page

--- course-distributor-frontend/src/pages/Register.tsx
+++ course-distributor-frontend/src/pages/Register.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import type { RegisterForm } from "../types/auth";
+import { register } from "../services/AuthService";
+import "../styles/Auth.css";
 
 const Register = () => {
 
-    const [registerForm, setRegisterForm] = useState<RegisterForm>({ email:'', password:''});
+    const [registerForm, setRegisterForm] = useState<RegisterForm>({ email:'', username:'', password:'', name: '', });
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 
@@ -16,28 +18,35 @@ const Register = () => {
 
     }
 
-    const handleSubmit= (e: React.SubmitEvent<HTMLFormElement>) => {
+    const handleSubmit= async(e: React.SubmitEvent<HTMLFormElement>) => {
 
         e.preventDefault();
 
         try{
-
+            const response = await register(registerForm);
+            console.log("Registration Successful! - ", response);
         }
-        catch{
-
+        catch(error){
+            console.log("Registration failed: ", error);
         }
 
     }
 
     return(
-        <div>
-            <h1>Register</h1>
-            <form onSubmit = {handleSubmit}>
+        <div className="auth-page">
+            <form className="auth-form" onSubmit = {handleSubmit}>
+                <h1 className="auth-title">Register</h1>
+                <div className="auth-group">
                 <label>Email</label>
                 <input type="text" value={registerForm.email} onChange={handleChange} name="email" />
                 <label>Password</label>
                 <input type="password" value={registerForm.password} onChange={handleChange} name="password" />
+                <label>Name</label>
+                <input type="text" value={registerForm.name} onChange={handleChange} name="name" />
+                <label>Username</label>
+                <input type="text" value={registerForm.username} onChange={handleChange} name="username" />
                 <button type="submit">Register</button>
+                </div>
             </form>
         </div>
     )
