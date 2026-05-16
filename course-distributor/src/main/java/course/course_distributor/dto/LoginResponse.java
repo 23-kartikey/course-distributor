c0d41c128d1bdcfdd44235f1efcd9209bcedb19d
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-16 19:26:19 +0530

    refactor: made token type field final

--- course-distributor/src/main/java/course/course_distributor/dto/LoginResponse.java
+++ course-distributor/src/main/java/course/course_distributor/dto/LoginResponse.java
@@ -1,18 +1,16 @@
 package course.course_distributor.dto;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
 public class LoginResponse{
     
     private String accessToken;
 
-    private String tokenType = "Bearer";
+    private final String tokenType = "Bearer";
 
 }
