b65ef4e4834e1e4102ea93aadfc84f5f8f134692
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-16 16:06:39 +0530

    feat: created authentication filter bean

--- course-distributor/src/main/java/course/course_distributor/security/JwtAuthenticationFilter.java
+++ course-distributor/src/main/java/course/course_distributor/security/JwtAuthenticationFilter.java
@@ -3,6 +3,12 @@ package course.course_distributor.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -12,4 +18,50 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request,
+                                HttpServletResponse response,
+                                FilterChain filterChain
+    ) throws IOException, ServletException {
+
+        String token = getTokenFromRequest(request);
+
+        if(StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)){
+
+            String username = jwtTokenProvider.getUsername(token);
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+
+
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request){
+
+        String bearerToken =  request.getHeader("Authorization");
+
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7, bearerToken.length());
+        }
+
+        return null;
+
+    }
+
+
+    
 }
