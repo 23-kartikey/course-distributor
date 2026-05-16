6aa8a5e43afd0f77461c52c0d075a1e923e672e5
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-16 15:59:38 +0530

    feat: add method to load user by username of email

--- course-distributor/src/main/java/course/course_distributor/security/CustomUserDetailsService.java
+++ course-distributor/src/main/java/course/course_distributor/security/CustomUserDetailsService.java
@@ -1,5 +1,35 @@
 package course.course_distributor.security;
 
-public class CustomUserDetailsService {
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import course.course_distributor.entity.User;
+import course.course_distributor.repository.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService{
     
+    private UserRepository userRepo;
+
+    public UserDetails loadUserByUsername(String usernameOrEmail){
+
+        User user = userRepo.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                            .orElseThrow(()->(new UsernameNotFoundException("User doesn't exist")));
+        
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
+                                .map(role->new SimpleGrantedAuthority(role.getName()))
+                                .collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                                                usernameOrEmail,
+                                                user.getPassword(),
+                                                authorities
+        );
+    }
+
 }
