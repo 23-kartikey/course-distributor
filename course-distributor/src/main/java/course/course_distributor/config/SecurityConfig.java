19e42d7ff7b5ca0be5c237eab775f915af0d8fbf
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-16 18:55:22 +0530

    feat: added custom SecurityFilterChain

--- course-distributor/src/main/java/course/course_distributor/config/SecurityConfig.java
+++ course-distributor/src/main/java/course/course_distributor/config/SecurityConfig.java
@@ -1,16 +1,51 @@
 package course.course_distributor.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import course.course_distributor.security.JwtAuthenticationEntryPoint;
+import course.course_distributor.security.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtFilter;
+    
+    @Autowired
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
     
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+
+        http
+            .csrf(csrf->csrf.disable())
+            .authorizeHttpRequests(auth->{
+                auth.requestMatchers("/auth/**").permitAll();
+                auth.requestMatchers("/public/**").permitAll();
+                auth.anyRequest().authenticated();
+            });
+
+        http.exceptionHandling(exception -> exception
+            .authenticationEntryPoint(authenticationEntryPoint)
+        );
+
+        http.addFilterBefore( jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+
+    }
+
 
 }
