ef3bc58c9b202500cfa0883be3e7c3971b0a9331
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-17 04:58:50 +0530

    feat: add slf4j logger

--- course-distributor/src/main/java/course/course_distributor/controller/AuthController.java
+++ course-distributor/src/main/java/course/course_distributor/controller/AuthController.java
@@ -1,8 +1,11 @@
 package course.course_distributor.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +18,17 @@ import course.course_distributor.interfaces.AuthService;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins="http://localhost:5173")
 public class AuthController {
+
+    private static final Logger logger= LoggerFactory.getLogger(AuthController.class);
     
     @Autowired
     private AuthService authService;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest req){
-
+        logger.info("=========REQUEST IS IN AUTHCONTROLLER===========");
         return ResponseEntity.status(HttpStatus.OK).body(authService.login(req));
 
     }
