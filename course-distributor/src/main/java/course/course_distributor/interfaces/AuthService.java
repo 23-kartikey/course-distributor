6efcede1ef6e7ec585bb645afbc7fad963929398
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-16 23:19:34 +0530

    feat: create interface method for registering users

--- course-distributor/src/main/java/course/course_distributor/interfaces/AuthService.java
+++ course-distributor/src/main/java/course/course_distributor/interfaces/AuthService.java
@@ -2,9 +2,11 @@ package course.course_distributor.interfaces;
 
 import course.course_distributor.dto.LoginRequest;
 import course.course_distributor.dto.LoginResponse;
+import course.course_distributor.dto.RegisterRequest;
 
 public interface AuthService {
     
     public LoginResponse login(LoginRequest req);
+    public void register(RegisterRequest req);
 
 }
