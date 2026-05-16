009324cbd3650be141b6bf554be3fef13955df1e
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-16 23:50:38 +0530

    feat: make the service access new public endpoint

--- course-distributor-frontend/src/services/CourseService.ts
+++ course-distributor-frontend/src/services/CourseService.ts
@@ -2,14 +2,14 @@ import API from "./api";
 import type { Course, CreateCourse } from "../types/course";
 
 export const getCourses = async ():Promise<Course[]> => {
-    const response = await API.get("/course");
+    const response = await API.get("public/course");
     return response.data;
 }
 
 export const createCourse = async( course : CreateCourse ):Promise<Course> => {
     
     const response = await API.post(
-        "/course",
+        "public/course",
         course
     )
 
