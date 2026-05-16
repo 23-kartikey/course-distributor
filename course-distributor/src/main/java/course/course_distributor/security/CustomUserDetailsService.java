9d55f28b0aa557cfaa84a5f9e11e2b01794772ab
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-16 16:07:48 +0530

    feat: marked this class as service for bean creation

--- course-distributor/src/main/java/course/course_distributor/security/CustomUserDetailsService.java
+++ course-distributor/src/main/java/course/course_distributor/security/CustomUserDetailsService.java
@@ -8,10 +8,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import course.course_distributor.entity.User;
 import course.course_distributor.repository.UserRepository;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService{
     
     private UserRepository userRepo;
