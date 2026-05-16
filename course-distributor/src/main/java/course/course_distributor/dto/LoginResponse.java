53c8ce5dd14220ebb9e5c8e636ec3785c8aed9bd
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-16 19:10:05 +0530

    feat: create DTO for login response

---
+++ course-distributor/src/main/java/course/course_distributor/dto/LoginResponse.java
@@ -0,0 +1,18 @@
+package course.course_distributor.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse{
+    
+    private String accessToken;
+
+    private String tokenType = "Bearer";
+
+}
