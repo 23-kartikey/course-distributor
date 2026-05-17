632b420a33fe6390de2c63338cd28af92a37587c
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-17 18:07:32 +0530

    feat: add CORS beanto enable global CORS configuration

--- course-distributor/src/main/java/course/course_distributor/config/SecurityConfig.java
+++ course-distributor/src/main/java/course/course_distributor/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package course.course_distributor.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import course.course_distributor.security.JwtAuthenticationEntryPoint;
 import course.course_distributor.security.JwtAuthenticationFilter;
@@ -32,10 +38,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
 
         http
+            .cors(cors->{})
             .csrf(csrf->csrf.disable())
             .authorizeHttpRequests(auth->{
                 auth.requestMatchers("/auth/**").permitAll();
                 auth.requestMatchers("/public/**").permitAll();
+                auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
                 auth.anyRequest().authenticated();
             });
 
@@ -53,6 +61,31 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfig){
         return authenticationConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(
+            List.of("http://localhost:5173")
+        );
+
+        configuration.setAllowedMethods(
+            List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        );
+        
+        configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+
+
     }
 
 }
