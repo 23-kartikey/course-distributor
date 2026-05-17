a9bfbf4978b01b6c02b20a7988f00592b6d0cdb9
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-17 18:06:35 +0530

    refactor: add functionality to accept user details :

--- course-distributor/src/main/java/course/course_distributor/controller/UserController.java
+++ course-distributor/src/main/java/course/course_distributor/controller/UserController.java
@@ -1,35 +1,45 @@
 package course.course_distributor.controller;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import course.course_distributor.dto.DetailsRequest;
 import course.course_distributor.dto.UserRequest;
 import course.course_distributor.dto.UserResponse;
 import course.course_distributor.service.UserService;
 
 @RestController
-@RequestMapping("/user")
+@CrossOrigin(origins="http://localhost:5173")
+@RequestMapping("public/user")
 public class UserController {
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserService service;
 
-    @GetMapping
-    public ResponseEntity<List<UserResponse>> getUsers(){
-        return ResponseEntity.ok(service.getUsers());
+    @PutMapping("/details/{id}")
+    public ResponseEntity<Void> fillDetails(@RequestBody DetailsRequest req, @PathVariable Long id){
+
+        logger.info("======IN FILLDETAILS CONTROLLER METHOD");
+
+        service.fillDetails(req, id);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
     }
 
-    @PostMapping
-    public ResponseEntity<UserResponse> newUser(@RequestBody UserRequest req ){
-
-        return ResponseEntity.status(201).body(service.newUser(req));
-
-    }
-    
 }
