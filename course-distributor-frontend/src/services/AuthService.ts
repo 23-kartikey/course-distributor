b225b36455fffeed23c4036ca358cf10ba6d4049
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-17 23:11:32 +0530

    feat: add token storage to service class

--- course-distributor-frontend/src/services/AuthService.ts
+++ course-distributor-frontend/src/services/AuthService.ts
@@ -4,13 +4,13 @@ import API from "./api";
 export const register = async(credentials:RegisterForm) => {
 
     const response = await API.post("/auth/register", credentials);
-
+    localStorage.setItem("token", response.data.token);
     return response.data;
 
 }
 
 export const login = async(credentials: LoginForm) => {
     const response = await API.post("/auth/login", credentials);
-
+    localStorage.setItem("token", response.data.token);
     return response.data;
 }
