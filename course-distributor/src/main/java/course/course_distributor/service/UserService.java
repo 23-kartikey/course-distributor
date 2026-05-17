c231ec6655c7cca4a7aa9b24c1c6df9eade43d46
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-17 18:08:47 +0530

    feat: add method to accept and persists user details

--- course-distributor/src/main/java/course/course_distributor/service/UserService.java
+++ course-distributor/src/main/java/course/course_distributor/service/UserService.java
@@ -1,12 +1,10 @@
 package course.course_distributor.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import course.course_distributor.dto.UserRequest;
-import course.course_distributor.dto.UserResponse;
+import course.course_distributor.dto.DetailsRequest;
 import course.course_distributor.entity.User;
 import course.course_distributor.repository.UserRepository;
 
@@ -16,27 +14,16 @@ public class UserService {
     @Autowired
     private UserRepository userRepo;
 
-    public List<UserResponse> getUsers(){
-        return userRepo.findAll().stream().map(this::toResponse).toList();
-    }
+    public void fillDetails(DetailsRequest req, Long id){
 
-    public UserResponse newUser(UserRequest req){
-        return toResponse(userRepo.save(toUser(req)));
-    }
+        User user = userRepo.findById(id).orElseThrow(()->new UsernameNotFoundException("User with id :"+id+" not found"));
+        user.setFirstName(req.firstName());
+        user.setLastName(req.lastName());
+        user.setUsername(req.username());
+        user.setAbout(req.about());
 
-    private User toUser(UserRequest req){
-        return User
-        .builder()
-        .name(req.name())
-        .build();
-    }
-    
-    private UserResponse toResponse(User user){
-        return UserResponse
-            .builder()
-            .id(user.getId())
-            .name(user.getName())
-            .build();
+        userRepo.save(user);
+
     }
 
 }
