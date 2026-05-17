eebbf4ca8fd4aafd544d9eea3751e53434eef3c5
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-17 18:06:56 +0530

    refactor: add register response as return type for register

--- course-distributor/src/main/java/course/course_distributor/interfaces/AuthService.java
+++ course-distributor/src/main/java/course/course_distributor/interfaces/AuthService.java
@@ -3,10 +3,11 @@ package course.course_distributor.interfaces;
 import course.course_distributor.dto.LoginRequest;
 import course.course_distributor.dto.LoginResponse;
 import course.course_distributor.dto.RegisterRequest;
+import course.course_distributor.dto.RegisterResponse;
 
 public interface AuthService {
     
     public LoginResponse login(LoginRequest req);
-    public void register(RegisterRequest req);
+    public RegisterResponse register(RegisterRequest req);
 
 }
