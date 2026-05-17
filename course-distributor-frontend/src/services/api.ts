4b57ff2bb0cad291ff9100b4f212b7cbdd937630
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-18 02:42:32 +0530

    feat: added interceptor to add token

--- course-distributor-frontend/src/services/api.ts
+++ course-distributor-frontend/src/services/api.ts
@@ -4,4 +4,16 @@ const API = axios.create({
     baseURL:"http://localhost:8080/"
 })
 
+API.interceptors.request.use((config) => {
+
+    const token = localStorage.getItem("token");
+
+    if(token){
+        config.headers.Authentication(`Bearer ${token}`);
+    }
+
+    return config;
+
+})
+
 export default API;
