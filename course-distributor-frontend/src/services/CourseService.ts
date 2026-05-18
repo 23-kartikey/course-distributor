77c4bece42fe1f06c237af488ad43f3c0e801777
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-19 05:09:40 +0530

    refactor: update getCourses function

--- course-distributor-frontend/src/services/CourseService.ts
+++ course-distributor-frontend/src/services/CourseService.ts
@@ -1,15 +1,23 @@
 import API from "./api";
 import type { Course, CreateCourse } from "../types/course";
+import type { PageResponse } from "../types/types";
 
-export const getCourses = async ():Promise<Course[]> => {
-    const response = await API.get("public/course");
+export const getCourses = async (page: number):Promise<PageResponse<Course>> => {
+
+    const response = await API.get("public/courses",
+        {
+            params: {
+                page, size: 6
+            }
+        }
+    );
     return response.data;
 }
 
 export const createCourse = async( course : CreateCourse ):Promise<Course> => {
     
     const response = await API.post(
-        "public/course",
+        "courses/new",
         course
     )
 
