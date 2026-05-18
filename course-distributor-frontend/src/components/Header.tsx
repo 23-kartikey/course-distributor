1b0dcc0e79ce979fab94a8e8fe875ab01dde8fc5
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-18 18:47:41 +0530

    feat: make the logo link to home page

--- course-distributor-frontend/src/components/Header.tsx
+++ course-distributor-frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 const Header = () =>{
     return(
         <div className="header-container">
-            <h1 className="header">CORE</h1>
+            <a href="/"><h1 className="header">CORE</h1></a>
             <Navbar />
         </div>
     )
