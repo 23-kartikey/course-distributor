0fc0b5e639e567ea179c163b972877e261251fd0
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-18 16:30:49 +0530

    feat: create seperate page for creating courses

---
+++ course-distributor-frontend/src/pages/CreateCourse.tsx
@@ -0,0 +1,16 @@
+import CourseForm from "../components/CourseForm";
+
+import "../styles/Courses.css";
+
+const CreateCourse = () =>{
+    
+
+    return(
+        <div className="courses-page">
+            <CourseForm />
+        </div>
+    );
+
+}
+
+export default CreateCourse;
