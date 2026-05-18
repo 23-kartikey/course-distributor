3cb88fecf0550286e1de36713aa09c8265e4f24d
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-18 18:48:11 +0530

    feat: tune the login logout buttons to switch on state

--- course-distributor-frontend/src/components/Navbar.tsx
+++ course-distributor-frontend/src/components/Navbar.tsx
@@ -1,15 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { logout } from "../services/AuthService";
 import "../styles/Navbar.css";
+import { useState } from "react";
 const Navbar = () => {
 
-    const token = localStorage.getItem("token");
+    const location = useLocation();
+
+    const isAuthPage = location.pathname === "/login" || 
+                       location.pathname === "/register";
+
+    const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+
+    const handleLogout = () => {
+        logout();
+        setIsLoggedIn(false);
+        window.location.href="/";
+    }
+
 
     return (
         <nav className="navbar">
             <Link className="nav-link" to="/">Home</Link>
             <Link className="nav-link" to="/courses">Courses</Link>
             <Link className="nav-link" to="/create-course">Create Course</Link>
-            {token?true:false && <Link className="nav-link" to="/login">Login/Register</Link>}
+            { !isAuthPage && 
+                (isLoggedIn ? (<button onClick={handleLogout} >Logout</button>)
+                    :(<Link className="nav-link" to="/login">Login/Register</Link>)
+                    )}
         </nav>
     );
 }
