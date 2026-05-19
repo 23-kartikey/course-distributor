ddee076508e1821c50f7878b9d4ef5c3fe9ab2d1
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-19 09:39:36 +0530

    feat: save multipart file

--- course-distributor/src/main/java/course/course_distributor/service/CourseService.java
+++ course-distributor/src/main/java/course/course_distributor/service/CourseService.java
@@ -1,9 +1,16 @@
 package course.course_distributor.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import course.course_distributor.dto.CourseRequest;
 import course.course_distributor.dto.CourseResponse;
@@ -21,10 +28,33 @@ public class CourseService {
         return courseRepo.findAll(pageable).map(this::toResponse);
     }
 
-    public CourseResponse newCourse(CourseRequest req, User user){
+    public CourseResponse newCourse(String name, 
+                                    String shortDescription, 
+                                    String description, 
+                                    MultipartFile thumbnail, 
+                                    User user) throws IOException{
         
-        return toResponse(courseRepo.save(toCourse(req, user)));
+        String fileName = UUID.randomUUID()
+                            +"_"
+                            +thumbnail.getOriginalFilename();
+
+        Path path = Paths.get("uploads", fileName);
+
+        Files.copy(
+            thumbnail.getInputStream(),
+            path
+        );
+
+        Course course = Course.builder()
+                                .name(name)
+                                .shortDescription(shortDescription)
+                                .description(description)
+                                .thumbnailUrl("/uploads/"+fileName)
+                                .author(user)
+                                .build();
         
+        return toResponse(courseRepo.save(course));
+
     }
     
     private Course toCourse(CourseRequest req, User user){
