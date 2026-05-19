6e006f88771780b00e35b7c34f7fb777696b3478
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-19 19:46:09 +0530

    fix: fixed nested hyperlinking

--- course-distributor-frontend/src/components/CourseCard.tsx
+++ course-distributor-frontend/src/components/CourseCard.tsx
@@ -8,15 +8,28 @@ type CourseCardProps = {
 
 const CourseCard = ({course}:CourseCardProps) => {
     return(
-        <a className = "course-card">
+        <div className = "course-card">
             <div className="course-banner">
+
+                <img
+                    src={
+                        `http://localhost:8080${course.thumbnailUrl}`
+                    }
+
+                    alt={course.name}
+
+                    className="banner-image"
+                    
+                />
+                <div className="banner-overlay" />
+
                 <h2>{course.name}</h2>
             </div>
             <div className = "course-content">
                 <p>{course.shortDescription}</p>
                 <p><a  className="author" href="/">Author</a></p>
             </div>
-        </a>
+        </div>
     )
 }
 
