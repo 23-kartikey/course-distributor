4913e66f0d0c52b890c7a831ced3bd0466a4669c
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-20 23:48:22 +0530

    feat: add username to DTO

--- course-distributor/src/main/java/course/course_distributor/dto/RegisterRequest.java
+++ course-distributor/src/main/java/course/course_distributor/dto/RegisterRequest.java
@@ -1,5 +1,5 @@
 package course.course_distributor.dto;
 
-public record RegisterRequest(String email, String password){
+public record RegisterRequest(String email, String password, String username){
     
 }
