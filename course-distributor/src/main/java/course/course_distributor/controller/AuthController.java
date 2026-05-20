7d154538e94b7711f39e91dc71440eb5fe1146d0
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-20 17:44:18 +0530

    feat: add endpoint to validate username

--- course-distributor/src/main/java/course/course_distributor/controller/AuthController.java
+++ course-distributor/src/main/java/course/course_distributor/controller/AuthController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import course.course_distributor.dto.LoginRequest;
@@ -39,6 +41,11 @@ public class AuthController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(req));
 
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<Boolean> checkUsername(@RequestParam String username){
+        return ResponseEntity.ok(authService.checkUsername(username));
     }
 
 }
