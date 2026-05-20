e63083837fbc0491a1189edee30be52b1278aca9
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-20 17:45:46 +0530

    feat: add method to check existence of username

--- course-distributor/src/main/java/course/course_distributor/repository/UserRepository.java
+++ course-distributor/src/main/java/course/course_distributor/repository/UserRepository.java
@@ -9,5 +9,5 @@ import course.course_distributor.entity.User;
 public interface UserRepository extends JpaRepository<User, Long>{
     
     public Optional<User> findByUsernameOrEmail(String username, String email);
-
+    public Boolean existsByUsername(String username);
 }
