6e3a8a365df9a79ca6d067d4c7a212af0f79c079
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-21 18:34:06 +0530

    feat: add function to get user profile

--- course-distributor-frontend/src/services/UserService.ts
+++ course-distributor-frontend/src/services/UserService.ts
@@ -1,8 +1,10 @@
 import API from "./api";
 import type { DetailsFormType } from "../types/auth";
 
-export const fillDetails = async(details: DetailsFormType) =>{
-    console.log("In here fillDetails function");
-    const response = await API.put(`public/user/details`, details);
+export const getUserProfile = async(id: number) => {
+
+    const response = await API.get(`public/user/${id}`);
+    console.log("Profile fetched");
     return response.data;
+
 }
