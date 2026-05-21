8b7524ab22d42bcda08aa5a384df11e23b00083c
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-21 07:18:33 +0530

    refactor: changed functionality to support auth context

--- course-distributor-frontend/src/services/AuthService.ts
+++ course-distributor-frontend/src/services/AuthService.ts
@@ -1,31 +1,21 @@
 import type { RegisterForm, LoginForm } from "../types/auth";
+import { useAuth } from "../contexts/AuthContext";
 import API from "./api";
 
 export const register = async(credentials:RegisterForm) => {
-
     const response = await API.post("/auth/register", credentials);
-    localStorage.setItem("token", response.data.token);
     return response.data;
 
 }
 
-export const login = async(credentials: LoginForm) => {
+export const loginUser = async(credentials: LoginForm) => {
     const response = await API.post("/auth/login", credentials);
-    localStorage.setItem("token", response.data.token);
     return response.data;
 }
 
-export const logout = () => {
-    localStorage.removeItem("token");
-    console.log("Logged out");
-}
 
 export const getToken = () => {
     return localStorage.getItem("token");
-}
-
-export const isLoggedIn = () => {
-    return !!getToken();
 }
 
 export const checkUsername = async(username:string):Promise<boolean> => {
