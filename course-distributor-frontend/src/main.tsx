4a6d8bdc24cda233ce23c551849f4e189d1c9ad9
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-21 07:20:09 +0530

    fix: fixed file type of import

--- course-distributor-frontend/src/main.tsx
+++ course-distributor-frontend/src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import AuthContext from './contexts/AuthContext.ts'
+import { AuthProvider } from './contexts/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider
+    <AuthProvider>
     <App />
-
+    </AuthProvider>
   </StrictMode>,
 )
