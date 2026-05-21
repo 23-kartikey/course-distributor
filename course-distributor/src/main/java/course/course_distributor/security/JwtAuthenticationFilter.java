f040cea43439d11c92541614f375aa4459427d4f
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-22 01:41:37 +0530

    refactor: changed parse of token to signed parse

--- course-distributor/src/main/java/course/course_distributor/security/JwtAuthenticationFilter.java
+++ course-distributor/src/main/java/course/course_distributor/security/JwtAuthenticationFilter.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -29,6 +30,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @PostConstruct
+    public void init(){
+        logger.info("JWT FILTER INITIALIZED");
+    }
 
     @Override
     public void doFilterInternal(HttpServletRequest request,
