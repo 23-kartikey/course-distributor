fe408a94a98e4e97412007d9fa32eaaffc6b08df
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-21 23:50:51 +0530

    feat: add useEffect hook to fetch userprofile at first render

--- course-distributor-frontend/src/pages/Profile.tsx
+++ course-distributor-frontend/src/pages/Profile.tsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+import type { UserProfileType } from "../types/user";
+import { getUserProfile } from "../services/UserService";
+
 const Profile = () => {
 
+    const [profile, setProfile] = useState<UserProfileType | null>(null);
+
+    useEffect(() => {
+        const fetchUserProfile = async() => {
+            try{
+                const response = await getUserProfile();
+                setProfile(response);
+            }
+            catch(error){
+                console.log("Couldn't fetch profile: ", error);
+            }
+        }
+    }, []);
 
     return(
         <div>
