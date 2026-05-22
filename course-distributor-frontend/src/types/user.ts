cc438e8678b5429c5ed694181f88474507a27b81
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-23 02:05:03 +0530

    feat: update types

--- course-distributor-frontend/src/types/user.ts
+++ course-distributor-frontend/src/types/user.ts
@@ -2,4 +2,12 @@ export type UserProfileType = {
     name : string;
     username: string;
     about: string;
+    profilePictureUrl?: string
+}
+
+export type EditProfileType = {
+    name: string;
+    username: string;
+    about: string;
+    profilePicture?: File;
 }
