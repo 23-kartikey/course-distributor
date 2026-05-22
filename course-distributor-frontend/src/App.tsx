42434d5df3dae37c01bbb5a5860bd48e1e95b4c5
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-23 02:00:27 +0530

    feat: add path to edit-profile page

--- course-distributor-frontend/src/App.tsx
+++ course-distributor-frontend/src/App.tsx
@@ -7,6 +7,7 @@ import Courses from "./pages/Courses";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
 
 const App = () => {
     return(
@@ -19,6 +20,7 @@ const App = () => {
                 <Route path="/register" element={<Register />} />
                 <Route path="/profile-user" element={<Profile />} />
                 <Route path="/create-course" element={<CreateCourse />} />
+                <Route path="/edit-profile" element={<EditProfile />} />
             </Routes>
         </BrowserRouter>
     );
