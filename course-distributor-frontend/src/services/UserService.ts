083e274bc09c1cd6d95884590f81a653134ee8b2
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-24 21:56:41 +0530

    chores: to include new type

--- course-distributor-frontend/src/services/UserService.ts
+++ course-distributor-frontend/src/services/UserService.ts
@@ -1,5 +1,5 @@
 import API from "./api";
-import type { UserProfileType } from "../types/user";
+import type { EditProfileType, UserProfileType } from "../types/user";
 
 export const getUserProfile = async():Promise<UserProfileType> => {
 
@@ -9,7 +9,15 @@ export const getUserProfile = async():Promise<UserProfileType> => {
 
 }
 
-export const editUserProfile = async(formData:FormData):Promise<UserProfileType> => {
+export const getEditUserProfile = async():Promise<EditProfileType> => {
+
+    const response = await API.get("user/profile/edit");
+    console.log("Profile fetched");
+    return response.data;
+
+}
+
+export const editUserProfile = async(formData:FormData):Promise<EditProfileType> => {
 
     const response = await API.put(
         "user/edit",
