726992a891ebf2c8bb36e42be11fb3ab99a8810d
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-06-03 23:22:20 +0530

    feat: add ListItem to return

--- course-distributor-frontend/src/pages/Followers.tsx
+++ course-distributor-frontend/src/pages/Followers.tsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
+import type { Follower } from "../types/user";
+import ListItem from "../components/ListItem";
 
 
 const Followers = ({id: number}) => {
 
-    const [followers, setFollowers] = useState<Followers[]>([]);
+    const [followers, setFollowers] = useState<Follower[]>([]);
+
+    return(
+        <ul>
+            {followers.map(follower => 
+                <ListItem key = {follower.id} item = {follower} />
+            )}
+        </ul>
+    );
 
 }
