5a53933199f0da3aca63d2ee8f209ac0006a68cc
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-06-03 23:43:20 +0530

    feat: add useParam and parse to number to get id

--- course-distributor-frontend/src/pages/Followers.tsx
+++ course-distributor-frontend/src/pages/Followers.tsx
@@ -2,9 +2,13 @@ import { useEffect, useState } from "react";
 import type { Follower } from "../types/user";
 import ListItem from "../components/ListItem";
 import { getFollowers } from "../services/UserService";
+import { useParams } from "react-router-dom";
 
 
-const Followers = (id: number) => {
+const Followers = () => {
+
+    const { idParam } = useParams();
+    const id = Number(idParam);
 
     const [followers, setFollowers] = useState<Follower[]>([]);
 
