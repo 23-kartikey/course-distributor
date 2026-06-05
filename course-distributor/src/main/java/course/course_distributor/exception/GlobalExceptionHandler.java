e62a367a5c52b87f644f3cdd08a4da4b688d3b71
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-06-06 00:57:59 +0530

    feat: add method to handle validation errors

--- course-distributor/src/main/java/course/course_distributor/exception/GlobalExceptionHandler.java
+++ course-distributor/src/main/java/course/course_distributor/exception/GlobalExceptionHandler.java
@@ -1,5 +1,28 @@
 package course.course_distributor.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors()
+                            .forEach(error -> 
+                                errors.put(error.getField(), error.getDefaultMessage())
+                            );
+        
+        return ResponseEntity.badRequest().body(errors);
+
+    }
+
 }
