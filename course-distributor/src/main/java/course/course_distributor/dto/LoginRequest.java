8ea0e33930ff745765b151034023ab1cfb9b1903
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-06-05 23:01:23 +0530

    feat: add validation constraints to Login request

--- course-distributor/src/main/java/course/course_distributor/dto/LoginRequest.java
+++ course-distributor/src/main/java/course/course_distributor/dto/LoginRequest.java
@@ -1,5 +1,14 @@
 package course.course_distributor.dto;
 
-public record LoginRequest(String usernameOrEmail, String password) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LoginRequest(
+    @NotBlank(message = "Username or Email required")
+    @Size(min=2, max=40)
+    String usernameOrEmail, 
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 50)
+    String password) {
     
 }
