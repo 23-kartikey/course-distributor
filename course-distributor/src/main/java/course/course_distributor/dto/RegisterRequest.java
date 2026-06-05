07374981cb232d5a7fffef922ebf98e99074e81e
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-06-05 23:01:46 +0530

    Feat: add validation to register request dto

--- course-distributor/src/main/java/course/course_distributor/dto/RegisterRequest.java
+++ course-distributor/src/main/java/course/course_distributor/dto/RegisterRequest.java
@@ -1,5 +1,17 @@
 package course.course_distributor.dto;
 
-public record RegisterRequest(String email, String password, String username){
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+    @NotBlank(message = "Email is required")
+    @Size(min = 4, max = 30)
+    String email, 
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 50)
+    String password, 
+    @NotBlank(message = "Username can't be empty")
+    @Size(min = 1, max = 50)
+    String username){
     
 }
