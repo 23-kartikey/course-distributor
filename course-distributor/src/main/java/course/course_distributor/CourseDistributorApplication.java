8d00ce204a32dde4178d309a641f31d2980bd458
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-06-06 15:17:01 +0530

    feat: add timezone by methods through the main method

--- course-distributor/src/main/java/course/course_distributor/CourseDistributorApplication.java
+++ course-distributor/src/main/java/course/course_distributor/CourseDistributorApplication.java
@@ -1,5 +1,7 @@
 package course.course_distributor;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseDistributorApplication {
 
 	public static void main(String[] args) {
+		System.out.println(TimeZone.getDefault());
+		System.out.println(System.getProperty("user.timezone"));
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+		System.setProperty("user.timezone", "Asia/Kolkata");
 		SpringApplication.run(CourseDistributorApplication.class, args);
 	}
 
