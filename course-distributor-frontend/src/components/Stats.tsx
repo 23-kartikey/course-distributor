b61ca946df18c7ad9902901256976930ac5e5d5c
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-06-08 04:06:53 +0530

    Feat: create stats component

---
+++ course-distributor-frontend/src/components/Stats.tsx
@@ -0,0 +1,27 @@
+import "../styles/Stats.css";
+
+export default function Stats() {
+  return (
+    <section className="stats">
+      <div>
+        <h1>50K+</h1>
+        <p>Students</p>
+      </div>
+
+      <div>
+        <h1>1200+</h1>
+        <p>Courses</p>
+      </div>
+
+      <div>
+        <h1>95%</h1>
+        <p>Success Rate</p>
+      </div>
+
+      <div>
+        <h1>4.8</h1>
+        <p>Rating</p>
+      </div>
+    </section>
+  );
+}
