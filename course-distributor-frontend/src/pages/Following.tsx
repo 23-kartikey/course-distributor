659ec2df88fff910e5c992ce40152971e1d2397c
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-06-07 15:40:35 +0530

    feat: create page for showing and fetching following

---
+++ course-distributor-frontend/src/pages/Following.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import type { Follower } from "../types/user";
+import ListItem from "../components/ListItem";
+import { getFollowers } from "../services/UserService";
+import { useParams } from "react-router-dom";
+
+
+const Following = () => {
+
+    const { id } = useParams();
+    const localId = Number(id);
+
+    const [followers, setFollowers] = useState<Follower[]>([]);
+
+    useEffect(()=>{
+        const loadFollowers = async(id: number) => {
+            try{
+                const response = await getFollowers(id);
+                setFollowers(response);
+                console.log("Followers fetched");
+            }
+            catch(error){
+                console.log("Couldn't fetch followers: ", error);
+            }
+        }
+        loadFollowers(localId);
+    }, []);
+
+    return(
+        <ul>
+            {followers.map(follower => 
+                <ListItem key = {follower.id} item = {follower} />
+            )}
+        </ul>
+    );
+
+}
+
+export default Following;
