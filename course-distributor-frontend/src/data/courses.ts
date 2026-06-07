be8fbf19153c7d949504a8905eb55d3cf2b2b20b
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-06-08 04:08:17 +0530

    feat: create data package

---
+++ course-distributor-frontend/src/data/courses.ts
@@ -0,0 +1,28 @@
+const courses = [
+  {
+    id: 1,
+    title: "Java Masterclass",
+    image:
+      "https://images.unsplash.com/photo-1515879218367-8466d910aaa4",
+    students: 1200,
+    rating: 4.9
+  },
+  {
+    id: 2,
+    title: "Spring Boot",
+    image:
+      "https://images.unsplash.com/photo-1555066931-4365d14bab8c",
+    students: 950,
+    rating: 4.8
+  },
+  {
+    id: 3,
+    title: "React Complete Guide",
+    image:
+      "https://images.unsplash.com/photo-1633356122544-f134324a6cee",
+    students: 1800,
+    rating: 5.0
+  }
+];
+
+export default courses;
