bbe76fc88e4cd984a7001e4f5ff143447c5cae8c
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-06-08 04:07:56 +0530

    feat: create CTA component

---
+++ course-distributor-frontend/src/components/CTA.tsx
@@ -0,0 +1,17 @@
+import "../styles/CTA.css";
+
+export default function CTA() {
+  return (
+    <section className="cta">
+      <h1>Ready To Start Learning?</h1>
+
+      <p>
+        Join thousands of students today.
+      </p>
+
+      <button>
+        Get Started
+      </button>
+    </section>
+  );
+}
